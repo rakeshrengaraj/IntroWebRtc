e845e237b938ca4d4e0213567db914e855df6cb9
Rakesh <rakesh@ptotem.com>
2020-05-14 18:21:43 +0530

    Message can be send to and for using express.io

--- server.js
+++ server.js
@@ -20,4 +20,11 @@ app.io.route('ready', function(req){
     })
 })
 
+app.io.route('send', function(req){
+    app.io.room(req.data.room).broadcast('startmessage', {
+        author: req.data.author,
+        message: req.data.message
+    })
+})
+
 app.listen(PORT);
